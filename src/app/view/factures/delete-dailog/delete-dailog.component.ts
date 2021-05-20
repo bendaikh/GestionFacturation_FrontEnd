e1974ad5239c232277f0f58ea4a41344283f6669
Oussama <oussamamoutiaa4@gmail.com>
2021-05-20 15:07:09 +0100

    Third Commit

---
+++ src/app/view/factures/delete-dailog/delete-dailog.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit , Inject} from '@angular/core';
+import {Facture} from '../../../controller/model/facture.model';
+import {FactureService} from '../../../controller/service/facture.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+@Component({
+  selector: 'app-delete-dailog',
+  templateUrl: './delete-dailog.component.html',
+  styleUrls: ['./delete-dailog.component.css']
+})
+export class DeleteDailogComponent implements OnInit {
+
+  // tslint:disable-next-line:max-line-length
+  constructor(private factureService: FactureService,  public dialogRef: MatDialogRef<FactureService>,  @Inject(MAT_DIALOG_DATA) public message: string ) { }
+  ngOnInit() {
+  }
+  onClickNo(): void {
+    this.dialogRef.close();
+  }
+
+}
---
+++ src/app/view/page-note-founded/page-note-founded.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-note-founded',
+  templateUrl: './page-note-founded.component.html',
+  styleUrls: ['./page-note-founded.component.css']
+})
+export class PageNoteFoundedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
